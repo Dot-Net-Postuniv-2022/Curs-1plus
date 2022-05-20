bb7646bc2ca932ad76b547f593c7de96440c6f3b
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2022-05-20 18:40:09 +0300

    Curs 3: add angular material and sql server database and migrations

--- TodoAngularApp/src/app/app-routing.module.ts
+++ TodoAngularApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {CounterComponent} from './components/counter/counter.component';
-import {TodoItemsComponent} from './components/todo-items/todo-items.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './components/counter/counter.component';
+import { TodoItemsComponent } from './components/todo-items/todo-items.component';
 
 const routes: Routes = [
   {
--- TodoAngularApp/src/app/app.module.ts
+++ TodoAngularApp/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {CounterComponent} from './components/counter/counter.component';
-import {TodoItemsComponent} from './components/todo-items/todo-items.component';
-import {HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { TodoItemsComponent } from './components/todo-items/todo-items.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -19,6 +23,11 @@ import {HttpClientModule} from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+
+    MatInputModule,
+    MatCheckboxModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
